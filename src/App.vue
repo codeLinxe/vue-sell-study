ce900c22d909451ae6a59530df94ab936ee25722
LinXH <896264217@qq.com>
2020-05-12 21:19:30 +0800

    v-header 静态页面创建

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app" @touchmove.prevent>
+		<v-header :seller="seller"></v-header>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	import VHeader from 'components/v-header/v-header'
+	
+	export default {
+		name: 'App',
+		components: {
+			VHeader
+		}
+	}
 </script>
 
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
 </style>

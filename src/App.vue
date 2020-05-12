fea91ec39e31f3d28c8462eda5a69e8c58b474e4
LinXH <896264217@qq.com>
2020-05-12 21:41:54 +0800

    v-header 数据获取和赋值

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
-	<div id="app" @touchmove.prevent>
-		<v-header :seller="seller"></v-header>
-	</div>
+  <div id="app" @touchmove.prevent>
+    <v-header :seller="seller"></v-header>
+  </div>
 </template>
 
 <script>
-	import VHeader from 'components/v-header/v-header'
-	
-	export default {
-		name: 'App',
-		components: {
-			VHeader
-		}
-	}
+  import VHeader from 'components/v-header/v-header'
+  import { getSeller } from 'api/index'
+
+  export default {
+    name: 'App',
+    components: {
+      VHeader
+    },
+    data() {
+      return {
+        seller: {}
+      }
+    },
+    created() {
+      getSeller().then((seller) => {
+        this.seller = seller
+      })
+    }
+  }
 </script>
 
 <style lang="stylus">

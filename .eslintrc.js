67aaea1b927dbab85db7dcfc060a9136efa3d345
LinXH <896264217@qq.com>
2020-05-13 21:32:15 +0800

    eslint rule 修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'space-before-function-paren': 0,
-    'indent': 0
+    'indent': 0,
+    'prefer-const': 0,
   },
   parserOptions: {
     parser: 'babel-eslint'
